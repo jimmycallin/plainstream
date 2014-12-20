7dabd7f4531db6dee8482a22193bc8a8c227150d
Jimmy Callin <jimmy.callin@gmail.com>
2014-12-20 15:25:00 +0100

    Implemented tokenizer

--- plainstream/plainstream.py
+++ plainstream/plainstream.py
@@ -3,18 +3,34 @@ import bz2
 from . import parser
 import sys
 
+try:
+    from .tokenizer import Tokenizer
+except ImportError:
+    pass
+
 available_languages = {'aa','ab','af','ak','als','am','an','ang','ar','arc','as','ast','av','ay','az','ba','bar','bcl','be','bg','bh','bi','bm','bn','bo','bpy','br','bs','bug','bxr','ca','cbk_zam','cdo','ce','ceb','ch','cho','chr','chy','co','cr','crh','cs','csb','cu','cv','cy','da','de','diq','dsb','dv','dz','ee','el','eml','en','eo','es','et','eu','ext','fa','ff','fi','fiu_vro','fj','fo','fr','frp','fur','fy','ga','gan','gd','gl','glk','gn','got','gu','gv','ha','hak','haw','he','hi','hif','ho','hr','hsb','ht','hu','hy','hz','ia','id','ie','ig','ii','ik','ilo','io','is','it','iu','ja','jbo','jv','ka','kaa','kab','kg','ki','kj','kk','kl','km','kn','ko','kr','ks','ksh','ku','kv','kw','ky','la','lad','lb','lbe','lg','li','lij','lmo','ln','lo','lt','lv','mdf','mg','mh','mi','mk','ml','mn','mo','mr','mt','mus','my','myv','mzn','na','nah','nap','nds','nds_nl','ne','new','ng','nl','nn','no','nov','nrm','nv','ny','oc','om','or','os','pa','pag','pam','pap','pdc','pi','pih','pl','pms','ps','pt','qu','quality','rm','rmy','rn','ro','roa_rup','roa_tara','ru','rw','sa','sah','sc','scn','sco','sd','se','sg','sh','si','simple','sk','sl','sm','sn','so','sr','srn','ss','st','stq','su','sv','sw','szl','ta','te','tet','tg','th','ti','tk','tl','tlh','tn','to','tpi','tr','ts','tt','tum','tw','ty','udm','ug','uk','ur','uz','ve','vec','vi','vls','vo','wa','war','wo','wuu','xal','xh','yi','yo','za','zea','zh','zh_classical','zh_min_nan','zh_yue','zu'}
 
-def get_text(language, max_bytes=None, max_words=None):
+available_outputs = {'plaintext'}
+
+
+def get_text(language, max_bytes=None, max_words=None, tokenize=False, train_sentence_tokenizer=False, output='plaintext'):
     """
     Returns a generator iteratively downloading text from Wikipedia dumps. 
     Parameters:
         language: Language code according to ISO_639-1, e.g. en, fr, sv, zh...
         max_bytes: Maximum number of bytes to download.
         max_words: Maximum number of words to download. Currently only works for languages with space-separated words.
+        tokenize: Tokenize the text into word units. Currently uses the Penn Trebank tokenizer and Punkt sentence segmenter, 
+                  and requires you to have NLTK installed. Outputted as one word per line, with two line breaks 
     """
     if language not in available_languages:
         raise RuntimeError("Language not supported.")
+
+    if tokenize:
+        train_text = None
+        if train_sentence_tokenizer:
+            train_text = get_text(language, max_words=10000, tokenize=False, train_sentence_tokenizer=False, output='plaintext')
+        tokenizer = Tokenizer(language, train_text_gen=train_text)
 
     dump_url = "http://dumps.wikimedia.org/{}wiki/latest/{}wiki-latest-pages-articles.xml.bz2".format(language, language)
     req = requests.get(dump_url, stream=True)
@@ -28,5 +44,9 @@ def get_text(language, max_bytes=None, max_words=None):
 
             nbytes += sys.getsizeof(line)
             nwords += len(line.split(" "))
-            yield line
-
+            if tokenize:
+                sentences = tokenizer.tokenize(line)
+                for sentence in sentences:
+                    yield sentence
+            else:
+                yield line
---
+++ plainstream/tokenizer.py
@@ -0,0 +1,32 @@
+
+from nltk.tokenize import PunktSentenceTokenizer, word_tokenize
+
+
+class Tokenizer(object):
+
+    def __init__(self, language, train_text_gen=None):
+        """
+        A tokenizer using NLTK Penn Treebank tokenizer, and the Punkt sentence tokenizer.
+        Params:
+        language: Language to tokenize (currently doesn't do anything)
+        train_text_gen: A generator of training text for the sentence tokenizer.
+        """
+        self.language = language
+
+        if train_text_gen:
+            self.sent_tokenizer = _train_sentence_tokenizer()
+        else:
+            self.sent_tokenizer = PunktSentenceTokenizer()
+
+    def _train_sentence_tokenizer(self):
+        return PunktSentenceTokenizer(train_text="\n".join(self.train_text_gen))
+
+    def tokenize(self, text):
+        tokenized = []
+        for sentence in self.sent_tokenizer.tokenize(text):
+            tokenized_sentence = []
+            for word in word_tokenize(sentence):
+                tokenized_sentence.append(word)
+            tokenized.append(tokenized_sentence)
+
+        return tokenized
